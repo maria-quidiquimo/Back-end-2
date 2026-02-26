caac10586d80a64c6cbab658c30d2ef616f202aa
maria-quidiquimo <maria.e.barreto12@aluno.senai.br>
2026-02-26 10:53:04 -0300

    fazendo o código para ver se o servidor está rodando

--- API-Cinema/src/server.js
+++ API-Cinema/src/server.js
@@ -1,6 +1,9 @@
 // importação das bibliotecas ou dos módulos aqui dentro.
 
 const pool = require('./config/database')
+const app = require('./app')
+
+const PORT = 3000
 
 //criando conexão
 pool.getConnection((err, connection) => {
@@ -8,6 +11,10 @@ pool.getConnection((err, connection) => {
         console.error('Erro ao conectar ao banco: ', err) // trás informação sobre o erro, ele já é mais voltado para erros no geral.
         process.exit(1)
     }
-    console.log('Conectado ao MySQL')
+    console.log('Conectado ao MySQL!')
     connection.release()
+})
+
+app.listen(PORT, () =>{
+    console.log('Servidor Rodando!')
 })
