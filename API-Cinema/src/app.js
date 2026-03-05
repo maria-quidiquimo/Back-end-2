34c3fcfd10d0db4181e870a67ee81229a8fe0556
maria-quidiquimo <maria.e.barreto12@aluno.senai.br>
2026-03-05 10:10:55 -0300

    modificações feitas na api cinema e feito o banco de dados no sql

--- API-Cinema/src/app.js
+++ API-Cinema/src/app.js
@@ -1,18 +1,65 @@
-// vai ter conexão com o banco de dados
+// // vai ter conexão com o banco de dados
+
+// const express = require('express')
+// const pool = require('./config/database') // puxando as informações da database.
+
+// const app = express()
+
+// app.use(express.json()) //tudo nos arquivos está trabalhando com arquivos json.
+
+// app.get('/', (req,res) =>{
+//     res.send("API Cinema")
+// })
+
+// app.get('/filmes', (req,res) =>{
+//     pool.query('SELECT * FROM filmes', (err, results) => {
+//         res.json(results)
+//     })
+// })
+
+// app.get('/filmes/:id', (req,res) => {
+//     const {id} = req.params
+
+//     pool.query('SELECT * FROM filme WHERE id = ?', [id],(err, results) =>{
+//         res.json(results)
+//     })
+// })
+
+
+// codigo professor
 
 const express = require('express')
-const pool = require('./config/database') // puxando as informações da database.
+const pool = require('./config/database')
 
 const app = express()
+app.use(express.json())
 
-app.use(express.json()) //tudo nos arquivos está trabalhando com arquivos json.
+const queryAsync = (sql, values = []) =>{
+    return new Promise((resolve, reject) => {
+        pool.query(sql, values, (err, results) =>{
+            if(err) reject(err)
+            else resolve(results)
+        })
+    })
+} //alinhando código js com banco
 
-app.get('/', (req,res) =>{
-    res.send("API Cinema")
+
+app.get('/', (req,res) => {
+    res.send("API CINEMA")
 })
 
-app.get('/filmes', (req,res) =>{
-    pool.query('SELECT * FROM filmes', (err, results) => {
+app.get('/filmes', (req,res) => {
+    pool.query('SELECT * FROM filme', (err, results) =>{
         res.json(results)
     })
 })
+
+app.get('/filmes/:id', (req,res) => {
+    const {id} = req.params
+
+    pool.query('SELECT * FROM filme WHERE id = ?', [id],(err, results) =>{
+        res.json(results)
+    })
+})
+
+module.exports = app
