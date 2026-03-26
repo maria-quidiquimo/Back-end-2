a14c44e1640c9eed763073a6bb3bcf5aeaaa827a
maria-quidiquimo <maria.e.barreto12@aluno.senai.br>
2026-03-26 08:01:00 -0300

    apagando o código antigo

--- API-Cinema/src/app.js
+++ API-Cinema/src/app.js
@@ -1,31 +1,3 @@
-// // vai ter conexão com o banco de dados
-
-// const express = require('express')
-// const pool = require('./config/database') // puxando as informações da database.
-
-// const app = express()
-
-// app.use(express.json()) //tudo nos arquivos está trabalhando com arquivos json.
-
-// app.get('/', (req,res) =>{
-//     res.send("API Cinema")
-// })
-
-// app.get('/filmes', (req,res) =>{
-//     pool.query('SELECT * FROM filmes', (err, results) => {
-//         res.json(results)
-//     })
-// })
-
-// app.get('/filmes/:id', (req,res) => {
-//     const {id} = req.params
-
-//     pool.query('SELECT * FROM filme WHERE id = ?', [id],(err, results) =>{
-//         res.json(results)
-//     })
-// })
-
-
 // codigo professor
 
 const express = require('express')
