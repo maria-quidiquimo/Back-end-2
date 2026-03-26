2ebcc0b1c7f75eabd707ad597ccb021c15037b77
maria-quidiquimo <maria.e.barreto12@aluno.senai.br>
2026-03-26 09:43:06 -0300

    fazendo os gets

--- Formativa-Maria-Eduarda-Quidiquimo-26_03/src/app.js
+++ Formativa-Maria-Eduarda-Quidiquimo-26_03/src/app.js
@@ -13,4 +13,66 @@ const queryAsync =(sql, values = []) =>{
     })
 }
 
+app.get('/', (req,res) =>{
+    res.send("API SABOR DIGITAL")
+})
+
+app.get('/produtos', async (req,res) =>{
+    try {
+        const produtos = await queryAsync ('SELECT * FROM produto')
+        res.json({
+            sucesso: true,
+            dados: produtos,
+            total: produtos.length
+        })
+    } catch (erro) {
+        console.error('Erro ao listar produto', erro)
+        res.status(500).json({
+            sucesso: false,
+            mensagem: 'Erro ao listar produtos',
+            erro: erro
+        })
+    }
+})
+
+app.get('/produtos/:id', (req, res) =>{
+    const {id} = req.params
+    pool.query('SELECT * FROM produto WHERE id = ?', [id], (err, results) =>{
+        res.json(results)
+    })
+})
+
+app.get('/produtos/:id', async (req, res) =>{
+    const {id} = req.params
+    try {
+        if(!id || isNaN (id)){
+            return res.status(400).json({
+                sucesso: false,
+                mensagem: 'ID Produto Inválido'
+            })
+        }
+        if(produto.length === 0){
+            return res.status(404).json({
+                sucesso: false,
+                mensagem: 'Produto não encontrado'
+            })
+        }
+        const produto = await queryAsync ('SELECT * FROM produto WHERE id = ?', [id])
+        res.json({
+            sucesso: true,
+            id: id,
+            dados: produto
+        })
+    } catch (erro) {
+        console.error ('Erro ao procurar Produto', erro)
+        res.status(500).json({
+            sucesso: false,
+            mensagem: 'Erro ao procurar Produto',
+            erro: erro.message
+        })
+    }
+})
+
+
+
 module.exports = app
