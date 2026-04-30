37e91bb1d80cf980f1839d519627ecbe529b66e4
maria-quidiquimo <maria.e.barreto12@aluno.senai.br>
2026-04-30 13:27:30 -0300

    fazendo a somativa 2

---
+++ Somativa-2-Maria-Quidiquimo/somativa-2-cleancode.js
@@ -0,0 +1,57 @@
+// =============================================================================
+// ROTA DE AGENDAMENTOS
+// =============================================================================
+
+app.put('reserva/:id', async (req, res) => {
+    try {
+        const {id} = req.params
+        const {sala, dia_reserva} = req.body
+        
+        const reservaExiste = await queryAsync("SELECT * FROM reservas WHERE id = ?", [id])
+        
+        if(reservaExiste.length === 0){
+            return res.status(404).json({
+                sucesso: false,
+                mensagem: "Erro ao criar sua reserva! Tente novamente."
+            })
+        }
+
+        if(sala != "Number" || sala <= 0){
+            res.status(400).json({
+                sucesso: false,
+                mensagem: "A Sala para sua reserva deve ser algum número positivo."
+            })
+        }
+
+        const reservaFoiAtualizada = {}
+
+        if (dia_reserva <= "Date") {
+            if(typeof dia_reserva != "Date" || dia_reserva <= "Date"){
+                res.status(400).json({
+                    sucesso: false,
+                    mensagem: "A data da reserva seve ser depois de hoje, e deve ser uma data válida."
+                })
+            } reservaFoiAtualizada.dia_reserva = dia_reserva
+        }
+
+
+        if (sala !== undefined) reservaFoiAtualizada.sala = sala.trim()
+
+        
+        await queryAsync("UPDATE reservas SET ? WHERE id = ?", [reservaFoiAtualizada, id])    
+            res.status(200).json({
+                sucesso: true,
+                mensagem: "Sua reserva foi atualizada com sucesso."
+            })
+            
+        } catch (erro) {
+            if(reservaExiste.length === 0){
+                res.status(404).json({
+                    sucesso: false,
+                    mensagem: "Reserva não foi feita. Tente novamente mais tarde."
+                })
+            }
+        }
+})
+
+// NOTA: Falta fazer o filtro de busca por data aqui
