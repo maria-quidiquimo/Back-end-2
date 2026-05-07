54c12c0c7abbbcef78644c62aa4bd1c76593250d
maria-quidiquimo <maria.e.barreto12@aluno.senai.br>
2026-05-07 08:49:09 -0300

    consegui arrumar isso

--- Formativa-Maria-Eduarda-Quidiquimo-26_03/src/services/produtoService.js
+++ Formativa-Maria-Eduarda-Quidiquimo-26_03/src/services/produtoService.js
@@ -33,6 +33,7 @@ class ProdutoService{
             sucesso: true,
             dados: produto
         }
+    }
 
     async cadastrarProduto(dados){
         const {nome, descricao, preco, categoria, disponivel} = dados
@@ -59,6 +60,90 @@ class ProdutoService{
             disponivel: disponivel || true
         }
 
+        const resultado = await ProdutoRepository.cadastrarProduto(novoProduto)
+
+        return{
+            sucesso: true,
+            mensagem: 'Produto cadastrado com sucesso',
+            resultado
+        }
     }
-}
+    async atualizarProduto(id, dados){
+        if(id || isNaN(id)){
+            throw{
+                status: 400,
+                mensagem: 'Id inválido.'
+            }
+        }
+
+        const produtoId = await ProdutoRepository.buscarProdutoPorId(id)
+
+        if(!produtoId){
+            throw{
+                status: 404,
+                mensagem: 'Produto não encontrado'
+            }
+        }
+
+        const produtoAtualizado = {}
+
+        const {nome, descricao, preco, categoria, disponivel} = dados
+
+        if(nome !== undefined && nome.trim() !== ' ') produtoAtualizado.nome = nome.trim()
+        
+        if(descricao !== undefined) produtoAtualizado.descricao =  descricao.trim()
+        
+        if(preco !== undefined){
+            if(typeof preco !== 'number' || preco <= 0){
+                throw{
+                    status: 400,
+                    mensagem: 'Preço deve ser um número positivo'
+                }
+            }
+        produtoAtualizado.preco = preco
+        } 
+
+        if(categoria !== undefined) produtoAtualizado.categoria = categoria
+
+        if(disponivel !== undefined) produtoAtualizado.disponivel = disponivel
+
+        if(Object.keys(produtoAtualizado).length === 0){
+            throw{
+                status: 400,
+                mensagem: 'Nenhum dado válido enviado para atualização.'
+            }
+        }
+
+        await ProdutoRepository.atualizarProduto(id, produtoAtualizado)
+
+        return{
+            sucesso: true,
+            mensagem: 'Produto atualizado.'
+        }
+    }
+
+    async deletarProduto(id){
+        if(id || isNaN(id)){
+            throw{
+                status: 400,
+                mensagem: 'Id inválido'
+            }
+        }
+
+        const idProduto = await ProdutoRepository.buscarProdutoPorId(id)
+
+        if(!idProduto){
+            throw{
+                status: 404,
+                mensagem: 'Produto não encontrado'
+            }
+        }
+
+        await ProdutoRepository.apagarProduto(id)
+
+        return{
+            sucesso: true,
+            mensagem: 'Produto apagado com sucesso'
+        }
+    }
 }
