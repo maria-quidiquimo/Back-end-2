28d8986403bd48798c69ce4fd2695b5ba4567386
maria-quidiquimo <maria.e.barreto12@aluno.senai.br>
2026-08-13 08:57:34 -0300

    mechendo na database e criando arquivos que talvez a gente use mais futuramente

--- sabor_digital-main(professor)/database.sql
+++ sabor_digital-main(professor)/database.sql
@@ -1,8 +1,8 @@
 -- Script de criação do banco de dados e tabelas - Cantina Bella Vita
 
 -- 1. Criação do Banco de Dados
-CREATE DATABASE IF NOT EXISTS sabordigital;
-USE sabordigital;
+CREATE DATABASE IF NOT EXISTS sabordigital_rep_celso;
+USE sabordigital_rep_celso;
 
 -- 2. Tabela de Produto (Prato/Bebida)
 CREATE TABLE IF NOT EXISTS produto (
