d6c93bfb240eb0d260c4b5a5989d02e3d96dca19
maria-quidiquimo <maria.e.barreto12@aluno.senai.br>
2026-08-20 09:46:52 -0300

    fazendo o usuario repository

---
+++ sabor_digital-main(professor)/src/repositories/UsuarioRepository.js
@@ -0,0 +1,46 @@
+const pool = require ('../config/database');
+
+class UsuarioRepository{
+    async findAll(){
+        const [rows] = await pool.query('SELECT * FROM usuario ORDER BY id DESC');
+        return rows;
+    }
+
+    async findById(id){
+        const [rows] = await pool.query('SELECT id, nome, email, papel, criado_em FROM usuario WHERE id = ?', [id]);
+        return rows[0];
+    }
+
+    async findByEmail(email){
+        const [rows] = await pool.query('SELECT * FROM usuario WHERE email = ?', [email]);
+        return rows[0];
+    }
+
+    async create(usuarioData){
+        const { nome, email, papel, criado_em } = usuarioData;
+        const [result] = await pool.query('INSERT INTO usuario (nome, email, papel) VALUES (?, ?, ?)', [nome, email, papel]);
+        return result.insertId
+    }
+
+    async update(id, usuarioData){
+        const fields = [];
+        const values = [];
+        for (const [key, value] of Object.entries(usuarioData)){
+            fields.push(`${key} = ?`);
+            values.push(value);
+        }
+        if(fields.length === 0) return null;
+
+        values.push(id);
+        const query = `UPDATE usuario SET ${fields.join(', ')} WHERE id = ?`;
+        const [result] = await pool.query(query, values);
+        return result.affectedRows;
+    }
+
+    async delete(id){
+        const [result] = await pool.query('DELETE FROM usuario WHERE id = ?', [id]);
+        return result.affectedRows
+    }
+}
+
+module.exports = new UsuarioRepository();
