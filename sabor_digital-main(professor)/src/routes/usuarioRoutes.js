01f520a194b1a94b1b33fb305f5a2742d5ca1fa0
maria-quidiquimo <maria.e.barreto12@aluno.senai.br>
2026-09-03 08:16:58 -0300

    Implementando o usuario Routes

--- sabor_digital-main(professor)/src/routes/usuarioRoutes.js
+++ sabor_digital-main(professor)/src/routes/usuarioRoutes.js
@@ -2,12 +2,13 @@ const express = require('express')
 const router = express.Router()
 const usuarioController = require('../controllers/UsuarioController')
 const upload = require('../config/multer')
+const{verificaToken, verificaAdmin} = require('../middlewares/authMiddlewares')
 
 
 router.get('/', usuarioController.listar)
 router.get('/:id', usuarioController.buscarPorId)
-// router.post('/', verificarToken, verificarAdmin, upload.single('imagem'), usuarioController.cadastrar)
-// router.put
-// router.delete('/:id', verificarToken,verificarAdmin, usuarioController.deletar)
+router.post('/', verificarToken, verificarAdmin, upload.single('imagem'), usuarioController.cadastrar)
+router.put('/:id', verificaToken, verificaAdmin, usuarioController.atualizar)
+router.delete('/:id', verificarToken,verificarAdmin, usuarioController.deletar)
 
 module.exports = router;
