e48e5ad9ed2b6150d47086d4ee3cefe5c2c0c5b6
maria-quidiquimo <maria.e.barreto12@aluno.senai.br>
2026-09-03 08:06:30 -0300

    Fazendo o usuário service de acordo com o modo que deve ser feito

--- sabor_digital-main(professor)/src/services/UsuarioService.js
+++ sabor_digital-main(professor)/src/services/UsuarioService.js
@@ -1,37 +1,51 @@
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
+const UsuarioRepository = require('../repositories/UsuarioRepository');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 
 const JWT_SECRET = process.env.JWT_SECRET || 'chave_super_secreta_sabor_digital_123';
-// class UsuarioService{
 
-// async Registrar usuário 
+class UsuarioService{
+    async RegistrarUsuario({nome, email, senha, papel}){
+        const usuarioExiste = await UsuarioRepository.findByEmail(email);
+        if (usuarioExiste){
+            return {sucesso: false, mensagem: 'Usuario já existe'};
+        }
 
-// criptografa a senha
-const salt = await bcrypt.getSalt(10);
-const senhaHash = await bcrypt.hash(senha, salt)
+        // criptografa a senha
+        const salt = await bcrypt.genSalt(10);
+        const senhaHash = await bcrypt.hash(senha, salt)
 
-// define o papel (se não for admin, por padrão vai ser cliente)
+        const role = (papel === 'admin') ? 'admin': 'cliente';
 
-const role = (papel === 'admin') ? 'admin': 'cliente';
+        const novoUsuario = await UsuarioRepository.create({
+            nome,
+            email,
+            senha: senhaHash,
+            papel: role
+        });
 
+        return{sucesso: true, usuario: novoUsuario}
+    }
 
-const novoId = await UsuarioRepository.create({
+    async Login(email, senha){
+        const usuario = await UsuarioRepository.findByEmail(email)
+        if(!usuario)
+            return{sucesso: false, mensagem: "Usuário não encontrado"}
 
-})
+        const senhaValida = await bcrypt.compare(senha, usuario.senha);
+        if(!senhaValida){
+            return{sucesso: false, mensagem: "Senha Inválida"}
+        }
 
-return{
-    sucesso : true,
-
+        const token = jwt.sign(
+        {id: usuario.id, email: usuario.email,
+        papel: usuario.papel},
+        JWT_SECRET,
+        {expiresIn: '8h'} // algorithm: 'ES256'
+        );
+        return {sucesso: true, token}
+    }
 }
-
-// async login
-
-const token = jwt.sign(
-    {id: usuario.id, email: usuario.email,
-    papel: usuario.papel},
-    JWT_SECRET,
-    {expiresIn: '8h'} // algorithm: 'ES256'
-);
 
 module.exports = new UsuarioService();
