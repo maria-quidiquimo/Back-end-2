6232da98c1222c609f18e13c75060cab8482c481
maria-quidiquimo <maria.e.barreto12@aluno.senai.br>
2026-09-10 08:37:15 -0300

    acho que vou deixar assim

--- Somativa-todo-list/todo_list/src/routes/usuarioRoutes.js
+++ Somativa-todo-list/todo_list/src/routes/usuarioRoutes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const UsuarioController = require('../controllers/UsuarioController');
 const { authMiddleware, authAdminMiddleware } = require('../middlewares/authMiddleware');
 
-router.post('/registrar', authMiddleware, UsuarioController.registrar);
-router.post('/login', authMiddleware ,UsuarioController.login);
-router.get('/', authMiddleware , UsuarioController.listar);
+router.post('/registrar',UsuarioController.registrar);
+router.post('/login' ,UsuarioController.login);
+router.get('/', UsuarioController.listar);
 router.delete('/:id', authAdminMiddleware, UsuarioController.deletar);
 
 module.exports = router;
