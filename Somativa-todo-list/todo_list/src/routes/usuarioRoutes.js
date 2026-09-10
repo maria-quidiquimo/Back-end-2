9cfdedc4d928debb33592476414c72d811197a76
maria-quidiquimo <maria.e.barreto12@aluno.senai.br>
2026-09-10 08:31:26 -0300

    tentei

--- Somativa-todo-list/todo_list/src/routes/usuarioRoutes.js
+++ Somativa-todo-list/todo_list/src/routes/usuarioRoutes.js
@@ -3,10 +3,9 @@ const router = express.Router();
 const UsuarioController = require('../controllers/UsuarioController');
 const { authMiddleware, authAdminMiddleware } = require('../middlewares/authMiddleware');
 
-
 router.post('/registrar', authMiddleware, UsuarioController.registrar);
 router.post('/login', authMiddleware ,UsuarioController.login);
 router.get('/', authMiddleware , UsuarioController.listar);
-router.delete('/:id', authAdminMiddleware , UsuarioController.deletar);
+router.delete('/:id', authAdminMiddleware, UsuarioController.deletar);
 
 module.exports = router;
