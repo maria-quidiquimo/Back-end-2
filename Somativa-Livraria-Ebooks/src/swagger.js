c8ef7abda97018f31f84c6186e01995bd2629c2c
maria-quidiquimo <maria.e.barreto12@aluno.senai.br>
2026-09-24 08:03:24 -0300

    fazendo o swagger com o professor

--- Somativa-Livraria-Ebooks/src/swagger.js
+++ Somativa-Livraria-Ebooks/src/swagger.js
@@ -8,20 +8,12 @@ const doc = {
     },
     host: 'localhost:3000',
     schemes: ['http'],
-
-    securityDefinitions: {
-        bearerAuth: {
-            type: 'http',
-            scheme: 'bearer',
-            bearerFormat: 'JWT'
-        }
-    }
 };
 
 const outputFile = './swagger_output.json';
 
-const endpointsFiles = ['./src/routes/index.js']
+const endpointsFiles = ['./routes/index.js']
 
 swaggerAutogen(outputFile, endpointsFiles, doc).then(() => {
-    console.log('Documentação do Swagger gerada com sucesso!')
+    console.log('Documentação gerada com sucesso!')
 });
