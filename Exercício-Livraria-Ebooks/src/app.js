1ff23002a67c5ecbe71beae0f906cbb293c73efe
maria-quidiquimo <maria.e.barreto12@aluno.senai.br>
2026-09-24 08:30:32 -0300

    nsei?

--- Exercício-Livraria-Ebooks/src/app.js
+++ Exercício-Livraria-Ebooks/src/app.js
@@ -2,14 +2,16 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const routes = require('./routes');
+
 const swaggerUi = require('swagger-ui-express');
-const swaggerFile = require('./swagger_output.json')
+const swaggerFile = require('./swagger_output.json');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use(cors());
 app.use(express.json());
 
 // Registro de rotas
-app.use('/', routes);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+app.use(routes);
 
 module.exports = app;
