573d30dcf2a4b26fcaba3f78b072751d3b8c6dc5
maria-quidiquimo <maria.e.barreto12@aluno.senai.br>
2026-09-24 08:56:06 -0300

    fazendo o app.js

--- Somativa-Loja-Jogos/src/app.js
+++ Somativa-Loja-Jogos/src/app.js
@@ -3,6 +3,11 @@ const cors = require('cors');
 const app = express();
 const routes = require('./routes');
 
+const swaggerUi = require('swagger-ui-express')
+const swaggerFiles = require('swagger_output.json')
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFiles))
+
 app.use(cors());
 app.use(express.json());
 
